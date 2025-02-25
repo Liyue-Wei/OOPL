23ec734bc3b90a2ae699fa0cd9f01465f038c5e4
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-02-25 17:46:11 +0800

    HW

---
+++ HomeWork/HW2_Ch03/HW.java
@@ -0,0 +1,12 @@
+package HomeWork.HW2_Ch03;
+
+public class HW {
+    public static void main(String[] args) {
+        double i = 10e5;
+        for(int n = 0; n < 5; n++) {
+            i*=1.02;
+        }
+
+        System.out.printf("%.0f%n", i);
+    }
+}
