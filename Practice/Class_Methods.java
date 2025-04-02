237df447b48ebbc6314e3fc7a9cce5de55d74ab8
Zhang Zhewei <126851525+Liyue-Wei@users.noreply.github.com>
2025-04-03 00:23:12 +0800

    C#寫到快瘋了

--- Practice/Class_Methods.java
+++ Practice/Class_Methods.java
@@ -5,7 +5,19 @@ public class Class_Methods {
         System.out.println("Hello World, from Java private static method!");
     }
 
+    public static void _printMSG() {
+        System.out.println("Hello World, from Java public static method!");
+    }
+
     public static void main(String[] args) {
         printMSG();
+        Class_Methods.printMSG();
+
+        _printMSG();
+        Class_Methods._printMSG();
+
+        // innerClass = new Class_Methods();
+        // innerClass.printMSG();
+        // innerClass._printMSG();
     }
 }
